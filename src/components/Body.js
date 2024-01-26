bd382bd6c9e689c0fa048530e94c5b241232310c
Shashikant Mishra <26shashimishra@gmail.com>
2024-01-26 20:51:12 +0530

    onAuthStateChanged  && title img

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,15 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { auth } from "../utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -13,6 +20,23 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  // This API acts as a event listener for the user in our app, and this needs to be done only once
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const { uid, email, displayName } = user;
+        console.log(uid);
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div>
       <RouterProvider router={appRouter} />
