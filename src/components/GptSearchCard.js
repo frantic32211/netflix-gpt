cfee67e9e3c05f79beccb735496d34108e813369
Shashikant Mishra <26shashimishra@gmail.com>
2024-02-10 20:14:45 +0530

    fetch searched movie data from TMDB

--- src/components/GptSearchCard.js
+++ src/components/GptSearchCard.js
@@ -2,14 +2,24 @@ import { useSelector } from "react-redux";
 import lang from "../utils/multiLanguage";
 import { useRef } from "react";
 import openai from "../utils/openAi";
+import { API_OPTIONS } from "../utils/constants";
 
 const GptSearchCard = () => {
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
 
-  const handleGptSearchClick = async () => {
-    console.log(searchText.current.value);
+  const searchMovieTmdb = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
 
+  const handleGptSearchClick = async () => {
     const gptQuery =
       "Act as a movie recommendation system and suggest some movies/web-series for the query : " +
       searchText.current.value +
@@ -20,9 +30,10 @@ const GptSearchCard = () => {
       messages: [{ role: "user", content: gptQuery }],
       model: "gpt-3.5-turbo",
     });
-    console.log(gptResults.choices);
 
-    if (!gptResults.choices)
+    if (
+      gptResults.choices?.[0]?.message?.content.match(/^(Sorry|Unfortunately)/g)
+    )
       return (
         <div className="bg-black bg-opacity-80">
           <h1 className="p-4 text-white text-3xl font-semibold">
@@ -30,7 +41,23 @@ const GptSearchCard = () => {
           </h1>
         </div>
       );
-    console.log(gptResults.choices?.[0]?.message?.content);
+
+    // DummyResult = Andaz Apna Apna, Hera Pheri, Golmaal: Fun Unlimited, 3 Idiots, Fukrey
+
+    // Split the array of movies = ['Andaz Apna Apna', 'Hera Pheri', 'Golmaal: Fun Unlimited', '3 Idiots', 'Fukrey']
+
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(", ");
+
+    console.log(gptMovies);
+    // For each movie, search on TMDB API
+
+    const movieData = gptMovies.map((movie) => searchMovieTmdb(movie));
+
+    // movieData will return array of 5 Promises (unresolved), because of async functionality of searchMovieTmdb
+
+    const TmdbResults = await Promise.all(movieData);
+
+    console.log(TmdbResults);
   };
 
   return (
